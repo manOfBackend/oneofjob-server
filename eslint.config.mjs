22fcc69795f574c11e97290eb320fd180c865e96
hyeonjong <man.of.backend@gmail.com>
2025-03-08 02:25:26 +0900

    fix: disable no-unsafe-return rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,7 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/require-await': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
