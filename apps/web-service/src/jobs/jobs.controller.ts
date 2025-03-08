323922af5f39677d33e9552db1a4e78a478aca3a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-08 21:12:50 +0900

    feat: Update JobsController

--- apps/web-service/src/jobs/jobs.controller.ts
+++ apps/web-service/src/jobs/jobs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 
 @Controller('jobs')
@@ -6,12 +6,26 @@ export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  async findAll(
+    @Query('career') career?: string,
+    @Query('company') company?: string,
+    @Query('employmentType') employmentType?: string,
+    @Query('period') period?: string,
+    @Query('title') title?: string,
+    @Query('url') url?: string,
+  ) {
+    return this.jobsService.findAll({
+      career,
+      company,
+      employmentType,
+      period,
+      title,
+      url,
+    });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.findOne(id);
   }
 }
