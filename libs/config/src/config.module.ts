8109f1465ac909e7a6ad7bed34aa199fec39900c
hyeonjong <man.of.backend@gmail.com>
2025-03-11 01:03:57 +0900

    feat: Enhance ConfigModule to support dynamic environment file loading

--- libs/config/src/config.module.ts
+++ libs/config/src/config.module.ts
@@ -3,28 +3,31 @@ import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import * as fs from 'fs';
 import { z } from 'zod';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { baseEnvSchema } from './config.schema';
 
 @Module({})
 export class ConfigModule {
-  static register(customSchema?: z.ZodTypeAny): DynamicModule {
+  static register(customSchema?: z.AnyZodObject): DynamicModule {
     dotenv.config();
 
-    const schema =
-      customSchema && customSchema instanceof z.ZodObject
-        ? baseEnvSchema.merge(customSchema)
-        : baseEnvSchema;
+    const schema = customSchema ? baseEnvSchema.merge(customSchema) : baseEnvSchema;
 
     const validate = (config: Record<string, unknown>) => {
       return schema.parse(config);
     };
 
+    const rootDir = process.cwd();
     const envFilePath: string[] = [];
-    if (fs.existsSync('.env')) {
-      envFilePath.push('.env');
+
+    const defaultEnvPath = path.join(rootDir, '.env');
+    if (fs.existsSync(defaultEnvPath)) {
+      envFilePath.push(defaultEnvPath);
     }
-    if (fs.existsSync(`.env.${process.env.NODE_ENV}`)) {
-      envFilePath.push(`.env.${process.env.NODE_ENV}`);
+
+    const envSpecificPath = path.join(rootDir, `.env.${process.env.NODE_ENV || 'development'}`);
+    if (fs.existsSync(envSpecificPath)) {
+      envFilePath.push(envSpecificPath);
     }
 
     return {
